9daa3b2dc653ba804a745ffb8e47e841a875a196
손진영 <sm099@naver.com>
2023-11-10 21:59:45 +0900

    test(step5): 이벤트 뱃지 생성 기능에 대한 테스트 코드 구현

---
+++ __tests__/domain/EventBadgeGenerator.test.js
@@ -0,0 +1,25 @@
+import EventBadgeGenerator from '../../src/domain/EventBadgeGenerator.js';
+
+describe('이벤트 뱃지 생성 테스트', () => {
+  test.each([
+    { totalRewardAmount: 5000, expectedBadge: '별' },
+    { totalRewardAmount: 10000, expectedBadge: '트리' },
+    { totalRewardAmount: 15000, expectedBadge: '트리' },
+    { totalRewardAmount: 20000, expectedBadge: '산타' },
+    { totalRewardAmount: 25000, expectedBadge: '산타' },
+    { totalRewardAmount: 4999, expectedBadge: null },
+    { totalRewardAmount: 0, expectedBadge: null },
+  ])(
+    '보상금액 $totalRewardAmount원에 대한 배지는 $expectedBadge 여야 한다',
+    ({ totalRewardAmount, expectedBadge }) => {
+      // given
+      const badgeGenerator = EventBadgeGenerator.from(totalRewardAmount);
+
+      // when
+      const badge = badgeGenerator.generateBadge();
+
+      // then
+      expect(badge === expectedBadge).toBeTruthy();
+    },
+  );
+});
