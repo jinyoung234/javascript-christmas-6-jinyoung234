701c39172476f26704bd1593b8bf53aae9b45b94
손진영 <sm099@naver.com>
2023-11-11 13:33:02 +0900

    test(MenuFinder): 함수형 모듈로 변경됨에 따른 테스트 코드 변경
    1. sampleMenuTable 제거
    
    2. sampleMenuTable 대신 실제 menuTable를 따르도록 테스트 케이스 변경
    
    3. 함수 호출 방식 변경

--- __tests__/domain/MenuFinder.test.js
+++ __tests__/domain/MenuFinder.test.js
@@ -1,33 +1,24 @@
-import MenuFinder from '../../src/domain/MenuFinder.js';
+import menuFinder from '../../src/domain/MenuFinder';
 
 describe('메뉴 찾기 테스트', () => {
-  const sampleMenuTable = {
-    메인: [{ name: '티본스테이크', price: 35000 }],
-    디저트: [{ name: '초코케이크', price: 7000 }],
-    음료: [{ name: '제로콜라', price: 2000 }],
-  };
-
   describe('findByMenuName 테스트', () => {
     test.each([
       {
         menuName: '티본스테이크',
         category: '스테이크',
-        expectedMenu: { name: '티본스테이크', price: 35000 },
+        expectedMenu: { name: '티본스테이크', price: 55000 },
       },
       {
         menuName: '초코케이크',
         category: '디저트',
-        expectedMenu: { name: '초코케이크', price: 7000 },
+        expectedMenu: { name: '초코케이크', price: 15000 },
       },
-      { menuName: '제로콜라', category: '음료', expectedMenu: { name: '제로콜라', price: 2000 } },
+      { menuName: '제로콜라', category: '음료', expectedMenu: { name: '제로콜라', price: 3000 } },
       { menuName: '초코케이크', category: '음료', expectedMenu: undefined },
     ])(
       '메뉴 이름 $menuName과 카테고리 $category로 메뉴를 찾으면 $expectedMenu를 반환해야 한다',
       ({ menuName, category, expectedMenu }) => {
-        // given
-        const menuFinder = MenuFinder.from(sampleMenuTable);
-
-        // when
+        // given - when
         const menu = menuFinder.findByMenuName(menuName, category);
 
         // then
@@ -45,10 +36,7 @@ describe('메뉴 찾기 테스트', () => {
     ])(
       '$menuName가 $category 카테고리에 속하는지 확인하면 $expectedResult를 반환해야 한다',
       ({ menuName, category, expectedResult }) => {
-        // given
-        const menuFinder = MenuFinder.from(sampleMenuTable);
-
-        // when
+        // given - when
         const result = menuFinder.isMenuInCategory(menuName, category);
 
         // then
