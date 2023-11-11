3e575653ebb90da3a3c93e0bec7283d9012fe40c
손진영 <sm099@naver.com>
2023-11-11 19:31:12 +0900

    refactor(EventPeriodChecker): 해당 모듈 제거 후 BenefitCalculation으로 로직 이동
    1. calculateBenefit, calculateChristmasDiscount에 isEventPeriod 로직 이동
    
    2. 불필요한 상수들 create 함수로 변경
    
    3. EventPeriodChecker의 테스트 코드 제거

--- src/domain/BenefitCalculation.js
+++ src/domain/BenefitCalculation.js
@@ -1,62 +1,30 @@
-import EventPeriodChecker from './EventPeriodChecker.js';
 import menuFinder from './MenuFinder.js';
-
-const INITIAL_BENEFIT_INFO = Object.freeze({
-  xmasDiscountAmount: 0,
-  weekDayDiscountAmount: 0,
-  weekendDiscountAmount: 0,
-  specialDiscountAmount: 0,
-  giftAmount: 0,
-});
-
-const EVENT_DATE_INFO = Object.freeze({
-  eventStartDate: new Date('2023-12-01'),
-  christmasDate: new Date('2023-12-25'),
-  eventEndDate: new Date('2023-12-31'),
-});
-
-const DAY_OF_DISCOUNT_INFO = Object.freeze({
-  weekday: Object.freeze({
-    menuCategory: '디저트',
-    days: [0, 1, 2, 3, 4],
-  }),
-
-  weekend: Object.freeze({
-    menuCategory: '메인',
-    days: [5, 6],
-  }),
-});
-
-export const SPECIAL_DATES = new Set([
-  '2023-12-03',
-  '2023-12-10',
-  '2023-12-17',
-  '2023-12-24',
-  '2023-12-25',
-  '2023-12-31',
-]);
-
-export const DISCOUNT_AMOUNTS = Object.freeze({
-  christmasDiscountPerDay: 100,
-  christmasInitialDiscount: 1000,
-  dayDiscount: 2023,
-  specialDiscount: 1000,
-  minimumOrderAmountForGift: 120000,
-});
 
 const benefitCalculation = Object.freeze({
   calculateBenefit(ordererInfo) {
-    const { eventStartDate: startDate, eventEndDate: endDate } = EVENT_DATE_INFO;
+    const [startDate, endDate] = [new Date('2023-12-01'), new Date('2023-12-31')];
+    const { visitDate } = ordererInfo;
 
-    if (!EventPeriodChecker.of(startDate, endDate).isEventPeriod(ordererInfo.visitDate))
-      return INITIAL_BENEFIT_INFO;
+    if (!(visitDate >= startDate && visitDate <= endDate)) return createInitialBenefitInfo();
 
     return createBenefitInfo(ordererInfo);
   },
 });
 
+export default benefitCalculation;
+
+function createInitialBenefitInfo() {
+  return Object.freeze({
+    xmasDiscountAmount: 0,
+    weekDayDiscountAmount: 0,
+    weekendDiscountAmount: 0,
+    specialDiscountAmount: 0,
+    giftAmount: 0,
+  });
+}
+
 function createBenefitInfo(ordererInfo) {
-  const { weekday, weekend } = DAY_OF_DISCOUNT_INFO;
+  const { weekday, weekend } = createDayOfDiscountInfo();
 
   return {
     xmasDiscountAmount: calculateChristmasDiscount(ordererInfo),
@@ -67,15 +35,29 @@ function createBenefitInfo(ordererInfo) {
   };
 }
 
-function calculateChristmasDiscount({ visitDate }) {
-  const { eventStartDate: startDate, christmasDate: endDate } = EVENT_DATE_INFO;
+function createDayOfDiscountInfo() {
+  return {
+    weekday: Object.freeze({
+      menuCategory: '디저트',
+      days: [0, 1, 2, 3, 4],
+    }),
 
-  if (!EventPeriodChecker.of(startDate, endDate).isEventPeriod(visitDate)) return 0;
+    weekend: Object.freeze({
+      menuCategory: '메인',
+      days: [5, 6],
+    }),
+  };
+}
+
+function calculateChristmasDiscount({ visitDate }) {
+  const [startDate, endDate] = [new Date('2023-12-01'), new Date('2023-12-25')];
+
+  if (!(visitDate >= startDate && visitDate <= endDate)) return 0;
 
   const millisecondPerDay = 1000 * 60 * 60 * 24;
   const daysUntilChristmas = Math.floor((endDate - visitDate) / millisecondPerDay);
 
-  const { christmasDiscountPerDay, christmasInitialDiscount } = DISCOUNT_AMOUNTS;
+  const [christmasDiscountPerDay, christmasInitialDiscount] = [100, 1000];
 
   const totalEventDay = 24;
   return christmasInitialDiscount + christmasDiscountPerDay * (totalEventDay - daysUntilChristmas);
@@ -86,25 +68,36 @@ function calculateDiscountForDayType({ ordererInfo: { menuInfo, visitDate }, men
 
   if (!days.includes(visitDay)) return 0;
 
+  const dayDiscount = 2023;
+
   return menuInfo
     .filter(([menuName]) => menuFinder.isMenuInCategory(menuName, menuCategory))
-    .reduce(
-      (totalDiscount, [, quantity]) => totalDiscount + DISCOUNT_AMOUNTS.dayDiscount * quantity,
-      0,
-    );
+    .reduce((totalDiscount, [, quantity]) => totalDiscount + dayDiscount * quantity, 0);
 }
 
 function calculateSpecialDiscount({ visitDate }) {
   const formattedVisitDate = visitDate.toISOString().substring(0, 10);
+  const specialDiscount = 1000;
+  const specialDates = createSpecialDates();
 
-  return SPECIAL_DATES.has(formattedVisitDate) ? DISCOUNT_AMOUNTS.specialDiscount : 0;
+  return specialDates.has(formattedVisitDate) ? specialDiscount : 0;
+}
+
+function createSpecialDates() {
+  return new Set([
+    '2023-12-03',
+    '2023-12-10',
+    '2023-12-17',
+    '2023-12-24',
+    '2023-12-25',
+    '2023-12-31',
+  ]);
 }
 
 function calculateGiftEvent({ totalOrderAmount }) {
-  if (totalOrderAmount < DISCOUNT_AMOUNTS.minimumOrderAmountForGift) return 0;
+  const minimumOrderAmountForGift = 120000;
+  if (totalOrderAmount < minimumOrderAmountForGift) return 0;
 
   const champagne = menuFinder.findByMenuName('샴페인', '음료');
   return champagne?.price ?? 0;
 }
-
-export default benefitCalculation;
