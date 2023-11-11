59ef78d49286ab0e684b6677c1b92184a974072b
손진영 <sm099@naver.com>
2023-11-11 12:22:20 +0900

    refactor(OutputView): print 메서드들의 전체적인 구조 리팩터링
    title 및 content를 출력하는 추상화 메서드 추가(printSection)
    
    2. 하드 코딩 된 값 상수화(OUTPUT_MESSAGE)
    
    3. 포매팅 관련 메서드들 분리(FORMAT_MESSAGE)
    
    4. 캡슐화를 위해 객체 바깥으로 선언 후 top-down 형식 표현을 위해 함수 표현식으로 변경

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -4,7 +4,17 @@ export const INPUT_MESSAGE = Object.freeze({
     '주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)\n',
 });
 
-export const FORMAT_MESSAGE = Object.freeze({
+export const OUTPUT_MESSAGE = Object.freeze({
+  title: Object.freeze({
+    orderMenu: '주문 메뉴',
+    totalOrderAmount: '할인 전 총주문 금액',
+    giftMenu: '증정 메뉴',
+    benefitHistory: '혜택 내역',
+    totalBenefitAmount: '총혜택 금액',
+    expectPaymentAmount: '할인 후 예상 결제 금액',
+    eventBadge: '12월 이벤트 배지',
+  }),
+
   discountLabels: Object.freeze({
     xmasDiscountAmount: '크리스마스 디데이 할인',
     weekDayDiscountAmount: '평일 할인',
@@ -12,4 +22,26 @@ export const FORMAT_MESSAGE = Object.freeze({
     specialDiscountAmount: '특별 할인',
     giftAmount: '증정 이벤트',
   }),
+});
+
+export const FORMAT_MESSAGE = Object.freeze({
+  orderMenus(menuInfo) {
+    return menuInfo.map(([menuName, quantity]) => `${menuName} ${quantity}개\n`).join('');
+  },
+
+  amount(amount) {
+    return `${amount.toLocaleString()}원`;
+  },
+
+  benefitHistory(benefitInfo) {
+    return Object.entries(benefitInfo)
+      .reduce(
+        (acc, [benefitName, amount]) =>
+          amount !== 0
+            ? [...acc, `${OUTPUT_MESSAGE.discountLabels[benefitName]}: -${this.amount(amount)}`]
+            : acc,
+        [],
+      )
+      .join('\n');
+  },
 });
--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -1,84 +1,66 @@
 import { Console } from '@woowacourse/mission-utils';
-import { FORMAT_MESSAGE } from '../constants/messages.js';
+import { FORMAT_MESSAGE, OUTPUT_MESSAGE } from '../constants/messages.js';
 
 const OutputView = {
-  printOrderMenu(menuInfo) {
-    Console.print('\n<주문 메뉴>');
-    const formatMenu = menuInfo
-      .map(([menuName, quantity]) => `${menuName} ${quantity}개\n`)
-      .join('');
-    Console.print(formatMenu);
-  },
-
-  printTotalOrderAmount(totalOrderAmount) {
-    Console.print('\n<할인 전 총주문 금액>');
-    Console.print(`${totalOrderAmount.toLocaleString()}원`);
-  },
-
-  printGiftMenu({ giftAmount }) {
-    Console.print('\n<증정 메뉴>');
-    if (giftAmount === 0) {
-      Console.print('없음');
-      return;
-    }
-    Console.print(`샴페인 1개`);
-  },
-
-  printRewardHistory(discountInfo) {
-    Console.print('\n<혜택 내역>');
-
-    const entries = Object.entries(discountInfo);
-    if (entries.every(([, amount]) => amount === 0)) {
-      Console.print('없음');
-      return;
-    }
-
-    entries.forEach(([key, amount]) => {
-      if (amount !== 0) {
-        Console.print(`${FORMAT_MESSAGE.discountLabels[key]}: -${amount.toLocaleString()}원`);
-      }
-    });
-  },
-
-  printTotalRewardAmount(totalRewardAmount) {
-    Console.print('\n<총혜택 금액>');
-    if (totalRewardAmount === 0) {
-      Console.print('없음');
-      return;
-    }
-    Console.print(`-${totalRewardAmount.toLocaleString()}원`);
-  },
-
-  printExpectPaymentAmount(expectPaymentAmount) {
-    Console.print('\n<할인 후 예상 결제 금액>');
-    if (expectPaymentAmount === 0) {
-      Console.print('없음');
-      return;
-    }
-    Console.print(`${expectPaymentAmount.toLocaleString()}원`);
-  },
-
-  printEventBadge(eventBadge) {
-    Console.print('\n<12월 이벤트 배지>');
-    if (!eventBadge) {
-      Console.print('없음');
-      return;
-    }
-    Console.print(eventBadge);
-  },
-
   printEventResult({
     menuInfo,
     eventResult: { totalOrderAmount, rewardAmountInfo, rewardInfo, eventBadge },
   }) {
-    this.printOrderMenu(menuInfo);
-    this.printTotalOrderAmount(totalOrderAmount);
-    this.printGiftMenu(rewardInfo);
-    this.printRewardHistory(rewardInfo);
-    this.printTotalRewardAmount(rewardAmountInfo.totalRewardAmount);
-    this.printExpectPaymentAmount(rewardAmountInfo.expectPaymentAmount);
-    this.printEventBadge(eventBadge);
+    printOrderMenu(menuInfo);
+    printTotalOrderAmount(totalOrderAmount);
+    printGiftMenu(rewardInfo);
+    printBenefitHistory(rewardInfo);
+    printTotalBenefitAmount(rewardAmountInfo.totalRewardAmount);
+    printExpectPaymentAmount(rewardAmountInfo.expectPaymentAmount);
+    printEventBadge(eventBadge);
   },
 };
 
 export default OutputView;
+
+function printOrderMenu(menuInfo) {
+  printSection(OUTPUT_MESSAGE.title.orderMenu, FORMAT_MESSAGE.orderMenus(menuInfo));
+}
+
+function printTotalOrderAmount(totalOrderAmount) {
+  printSection(OUTPUT_MESSAGE.title.totalOrderAmount, FORMAT_MESSAGE.amount(totalOrderAmount), {
+    newLine: false,
+  });
+}
+
+function printGiftMenu({ giftAmount }) {
+  printSection(OUTPUT_MESSAGE.title.giftMenu, giftAmount !== 0 ? '샴페인 1개' : null);
+}
+
+function printBenefitHistory(benefitInfo) {
+  printSection(
+    OUTPUT_MESSAGE.title.benefitHistory,
+    FORMAT_MESSAGE.benefitHistory(benefitInfo) || null,
+  );
+}
+
+function printTotalBenefitAmount(totalRewardAmount) {
+  printSection(
+    OUTPUT_MESSAGE.title.totalBenefitAmount,
+    totalRewardAmount !== 0 ? `-${FORMAT_MESSAGE.amount(totalRewardAmount)}` : null,
+  );
+}
+
+function printExpectPaymentAmount(expectPaymentAmount) {
+  printSection(
+    OUTPUT_MESSAGE.title.expectPaymentAmount,
+    expectPaymentAmount !== 0 ? `${FORMAT_MESSAGE.amount(expectPaymentAmount)}` : null,
+  );
+}
+
+function printEventBadge(eventBadge) {
+  printSection(OUTPUT_MESSAGE.title.eventBadge, eventBadge);
+}
+
+function printSection(title, content, config = { newLine: true }) {
+  Console.print(`${config.newLine ? '\n' : ''}<${title}>`);
+
+  if (content) Console.print(content);
+
+  if (!content) Console.print('없음');
+}
