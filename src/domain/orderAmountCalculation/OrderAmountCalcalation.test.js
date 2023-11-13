7e403edac32dc7d0162227639b6e54dc5f002434
손진영 <sm099@naver.com>
2023-11-13 21:17:17 +0900

    test(orderAmountCalculation): 메뉴 테이블에 없는 메뉴에 대한 테스트 케이스 추가

--- src/domain/orderAmountCalculation/OrderAmountCalcalation.test.js
+++ src/domain/orderAmountCalculation/OrderAmountCalcalation.test.js
@@ -17,6 +17,10 @@ describe('총 주문 금액 계산 테스트', () => {
       ],
       expected: 66000,
     },
+    {
+      menuInfo: [['새송이수프', 2]],
+      expected: 0,
+    },
   ])(
     '메뉴 정보를 통해 할인 전 주문한 총 금액은 $expected원 이어야 한다',
     ({ menuInfo, expected }) => {
