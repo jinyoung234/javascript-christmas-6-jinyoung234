58f8aa2464c76f0e52687aaa5c5c02dc932b28a9
손진영 <sm099@naver.com>
2023-11-14 11:42:41 +0900

    refactor(PROMOTION_MENU_TABLE): 각 메뉴의 price에 numeric operator 추가

--- src/constants/promotionSystem.js
+++ src/constants/promotionSystem.js
@@ -7,57 +7,57 @@ export const PROMOTION_MENU_TABLE = Object.freeze({
   애피타이저: [
     {
       name: '양송이수프',
-      price: 6000,
+      price: 6_000,
     },
     {
       name: '타파스',
-      price: 5500,
+      price: 5_500,
     },
     {
       name: '시저샐러드',
-      price: 8000,
+      price: 8_000,
     },
   ],
   메인: [
     {
       name: '티본스테이크',
-      price: 55000,
+      price: 55_000,
     },
     {
       name: '바비큐립',
-      price: 54000,
+      price: 54_000,
     },
     {
       name: '해산물파스타',
-      price: 35000,
+      price: 35_000,
     },
     {
       name: '크리스마스파스타',
-      price: 25000,
+      price: 25_000,
     },
   ],
   디저트: [
     {
       name: '초코케이크',
-      price: 15000,
+      price: 15_000,
     },
     {
       name: '아이스크림',
-      price: 5000,
+      price: 5_000,
     },
   ],
   음료: [
     {
       name: '제로콜라',
-      price: 3000,
+      price: 3_000,
     },
     {
       name: '레드와인',
-      price: 60000,
+      price: 60_000,
     },
     {
       name: '샴페인',
-      price: 25000,
+      price: 25_000,
     },
   ],
 });
