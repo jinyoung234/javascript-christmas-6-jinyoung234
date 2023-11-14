0c881274d9c9442b99ae661a2002236515b19f12
손진영 <sm099@naver.com>
2023-11-14 12:38:53 +0900

    test(messages): given - when - then 패턴 추가

--- src/constants/messages/Messages.test.js
+++ src/constants/messages/Messages.test.js
@@ -12,6 +12,7 @@ describe('메시지 포맷 테스트', () => {
         output: '피자 2개\n콜라 1개\n',
       },
     ])('$description', ({ input, output }) => {
+      // given - when - then
       expect(FORMAT_MESSAGE.orderMenus(input)).toBe(output);
     });
   });
@@ -20,6 +21,7 @@ describe('메시지 포맷 테스트', () => {
     test.each([{ input: 5000, output: '5,000원' }])(
       'amount가 $input일 때, 출력 메시지는 $output이어야 한다',
       ({ input, output }) => {
+        // given - when - then
         expect(FORMAT_MESSAGE.amount(input)).toBe(output);
       },
     );
@@ -31,15 +33,16 @@ describe('메시지 포맷 테스트', () => {
         description:
           '출력 결과는 "크리스마스 디데이 할인: -1,200원 - 평일 할인: -4,046원 - 특별 할인: -1,000원 - 증정 이벤트: -25,000원"이다.',
         input: {
-          xmasDiscountAmount: 1200,
-          weekDayDiscountAmount: 4046,
-          specialDiscountAmount: 1000,
+          xmasBenefitAmount: 1200,
+          weekDayBenefitAmount: 4046,
+          specialBenefitAmount: 1000,
           giftAmount: 25000,
         },
         output:
           '크리스마스 디데이 할인: -1,200원\n평일 할인: -4,046원\n특별 할인: -1,000원\n증정 이벤트: -25,000원',
       },
     ])('$description', ({ input, output }) => {
+      // given - when - then
       expect(FORMAT_MESSAGE.benefitHistory(input)).toBe(output);
     });
   });
@@ -57,6 +60,7 @@ describe('메시지 포맷 테스트', () => {
         output: '\n<제목>',
       },
     ])('$description', ({ input, output }) => {
+      // given - when - then
       expect(FORMAT_MESSAGE.title(input.config, input.title)).toBe(output);
     });
   });
